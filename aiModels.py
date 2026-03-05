81df67ba28407a073431263391a113f7d123cdcd
rep467 <24.per.sander@gmail.com>
2026-03-05 16:17:05 -0500

    moved models out of train-cnn code

---
+++ aiModels.py
@@ -0,0 +1,66 @@
+import torch
+
+class CNN1(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=3, out_channels=32,
+                            kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.Conv2d(in_channels=32, out_channels=64,
+                            kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.Conv2d(in_channels=64, out_channels=64,
+                            kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(64 * 16 * 16, 2048),
+            torch.nn.ReLU(),
+            torch.nn.Linear(2048, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class CNN2(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.nn.Sequential(
+                torch.nn.Conv2d(3, 64, 3, padding=1),
+                torch.nn.BatchNorm2d(64),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(2),
+
+                # Block 2
+                torch.nn.Conv2d(64, 128, 3, padding=1),
+                torch.nn.BatchNorm2d(128),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(2),
+
+                # Block 3
+                torch.nn.Conv2d(128, 256, 3, padding=1),
+                torch.nn.BatchNorm2d(256),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(2),
+
+                # Block 4
+                torch.nn.Conv2d(256, 512, 3, padding=1),
+                torch.nn.BatchNorm2d(512),
+                torch.nn.ReLU(),
+                torch.nn.AdaptiveAvgPool2d((1, 1)),
+
+                torch.nn.Flatten(),
+                torch.nn.Dropout(0.5),
+                torch.nn.Linear(512, 512),
+
+                torch.nn.ReLU(),
+                torch.nn.Dropout(0.5),
+                torch.nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
--- train-cnn.py
+++ train-cnn.py
@@ -7,39 +7,20 @@ from torch.utils.data import random_split
 
 from dataset import *
 from performanceTester import *
+from aiModels import *
 
-batch_size = 512
+# increase to speed up training (this is limited by vram but also depends on the model)
+batch_size = 64
+
+# increase this to nr of logical threads of your machine to speed up training
+num_workers = 0
 torch.manual_seed(42)
 
 train_dataset, test_dataset, classes = LoadAnimals10Dataset(batch_size=batch_size, seed=42)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=0)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, num_workers=0)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
 
-class CNN(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=3, out_channels=32,
-                            kernel_size=3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2),
-            torch.nn.Conv2d(in_channels=32, out_channels=64,
-                            kernel_size=3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2),
-            torch.nn.Conv2d(in_channels=64, out_channels=64,
-                            kernel_size=3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2),
-            torch.nn.Flatten(),
-            torch.nn.Linear(64 * 16 * 16, 2048),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2048, 10)
-        )
-
-    def forward(self, x):
-        return self.model(x)
 
 device = 'cpu'
 
@@ -49,12 +30,12 @@ if torch.cuda.is_available():
 else:
     print("gpu not available")
 
-model = CNN().to(device)
+model = CNN2().to(device)
 
 for images, labels in train_loader:
     images, labels = images.to(device), labels.to(device)
 
-num_epochs = 50
+num_epochs = 100
 learning_rate = 0.0001
 weight_decay = 0.01
 criterion = torch.nn.CrossEntropyLoss()
@@ -79,4 +60,8 @@ for epoch in range(num_epochs):
     train_loss_list.append(train_loss / len(train_loader))
     print(f"Training loss = {train_loss_list[-1]}")
 
-    CalculatePerformanceMetrics(model, test_loader, classes, device, True)
+    if (epoch + 1) % 10 == 0:
+        print("Performance Testing:")
+        CalculatePerformanceMetrics(model, test_loader, classes, device, True)
+        print("Performance Training:")
+        CalculatePerformanceMetrics(model, train_loader, classes, device, True)
